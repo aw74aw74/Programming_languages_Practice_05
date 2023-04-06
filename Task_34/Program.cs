b858ef803fee221832090db5ad5b9b4821752335
aw74aw74 <aw74@ya.ru>
2023-04-06 17:40:04 +0500

    Здача 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,33 @@
+﻿//---------------------ЗАДАЧА---------------------
+/*
+Задача 34: Задайте массив заполненный случайными положительными
+трёхзначными числами. Напишите программу, которая покажет количество
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+//---------------------РЕШЕНИЕ---------------------
+
+int[] FillArray(int[] col, int min, int max)
+{
+  for (int index = 0; index < col.Length; index++)
+  {
+    col[index] = new Random().Next(min, max);
+  }
+  return col;
+}
+
+int SearchArray(int[] col)
+{
+  int res = 0;
+  foreach (int el in col)
+  {
+    if (el % 2 == 0) res ++;
+  }
+  return res;
+}
+
+int[] array = new int[10];
+FillArray(array, 100, 1000);
+Console.WriteLine($"Дан массив: {String.Join(" ", array)}.");
+Console.WriteLine($"Количество четных чисел: {SearchArray(array)}.");
